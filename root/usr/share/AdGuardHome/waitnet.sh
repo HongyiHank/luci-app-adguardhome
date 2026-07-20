b2a5c65bd8207da180b5667489feb285bc232c67
HongyiHank <github@hhdoubleh.us.to>
2026-07-20 09:21:37 +0800

    改善 waitnet.sh：優先嘗試國際通用目標 1.1.1.1/8.8.8.8，保留 baidu.com 為中國用戶備援，移除 sleep 5 在 ping 之間以加快反應

--- root/usr/share/AdGuardHome/waitnet.sh
+++ root/usr/share/AdGuardHome/waitnet.sh
@@ -1,34 +1,16 @@
 #!/bin/sh
 PATH="/usr/sbin:/usr/bin:/sbin:/bin"
 count=0
-while :
-do
-	ping -c 1 -W 1 -q www.baidu.com 1>/dev/null 2>&1
-	if [ "$?" == "0" ]; then
-		/etc/init.d/AdGuardHome force_reload
-		break
-	fi
-	ping -c 1 -W 1 -q 202.108.22.5 1>/dev/null 2>&1
-	if [ "$?" == "0" ]; then
-		/etc/init.d/AdGuardHome force_reload
-		break
-	fi
-	sleep 5
-	ping -c 1 -W 1 -q www.google.com 1>/dev/null 2>&1
-	if [ "$?" == "0" ]; then
-		/etc/init.d/AdGuardHome force_reload
-		break
-	fi
-	ping -c 1 -W 1 -q 8.8.8.8 1>/dev/null 2>&1
-	if [ "$?" == "0" ]; then
-		/etc/init.d/AdGuardHome force_reload
-		break
-	fi
-	sleep 5
+while :; do
+	for target in "1.1.1.1" "8.8.8.8" "www.google.com" "www.baidu.com"; do
+		ping -c 1 -W 1 -q "$target" >/dev/null 2>&1 && {
+			/etc/init.d/AdGuardHome force_reload
+			exit 0
+		}
+	done
 	count=$((count+1))
-	if [ $count -gt 18 ]; then
-		/etc/init.d/AdGuardHome force_reload
-		break
-	fi
+	[ $count -gt 18 ] && break
+	sleep 5
 done
+/etc/init.d/AdGuardHome force_reload
 exit 0

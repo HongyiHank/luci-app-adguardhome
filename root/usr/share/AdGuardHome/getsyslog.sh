7e156a90c3e2fe94e7853e5ee3f8dee7abc962f2
HongyiHank <github@hhdoubleh.us.to>
2026-07-20 09:21:10 +0800

    修復 getsyslog.sh watchdog race：改 3s 輪詢 + 4 次連續未命中才退出，避免因 AdG_syslog 競爭寫入誤殺 logread

--- root/usr/share/AdGuardHome/getsyslog.sh
+++ root/usr/share/AdGuardHome/getsyslog.sh
@@ -4,16 +4,21 @@ logread -e AdGuardHome > /tmp/AdGuardHome.log
 logread -e AdGuardHome -f >> /tmp/AdGuardHome.log &
 pid=$!
 echo "1">/var/run/AdG_syslog
+misses=0
 while true
 do
-	sleep 12
+	sleep 3
 	watchdog=$(cat /var/run/AdG_syslog)
 	if [ "$watchdog"x == "0"x ]; then
-		kill $pid
-		rm /tmp/AdGuardHome.log
-		rm /var/run/AdG_syslog
-		exit 0
+		misses=$((misses+1))
+		if [ $misses -ge 4 ]; then
+			kill $pid 2>/dev/null
+			rm -f /tmp/AdGuardHome.log
+			rm -f /var/run/AdG_syslog
+			exit 0
+		fi
 	else
+		misses=0
 		echo "0">/var/run/AdG_syslog
 	fi
 done
